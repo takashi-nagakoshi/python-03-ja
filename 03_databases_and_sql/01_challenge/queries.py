d3a27d1efb8ee82c132900c463302ab11a71caca
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-07-08 18:33:14 +0900

    queries

--- 03_databases_and_sql/01_challenge/queries.py
+++ 03_databases_and_sql/01_challenge/queries.py
@@ -9,38 +9,51 @@ db = conn.cursor()
 
 # アーティストの数
 def number_of_artists(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT COUNT(ArtistId) FROM albums a;"  # ここにSQLクエリを書いてください
     db.execute(query)
+    # rows = db.fetchall()
+    # print(rows)
     results = db.fetchall()
-    return results
+    return results[0] if results else 0
 
 # アーティストのリスト
 def list_of_artists(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT * FROM artists ORDER BY Name ASC"  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
 # 「愛」をテーマにしたアルバムのリスト
 def albums_about_love(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT * FROM Tracks WHERE Name LIKE '%love%' ORDER BY Name ASC;"  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
 # 指定された再生時間よりも長い楽曲数
 def tracks_longer_than(db, duration):
-    query = ""  # ここにSQLクエリを書いてください
-    db.execute(query)
+    query = "SELECT COUNT(TrackId) FROM tracks WHERE Milliseconds > ?"  # ここにSQLクエリを書いてください
+    db.execute(query,(duration,))
     results = db.fetchall()
-    return results
+    return results[0] if results else 0
 
 # 最も楽曲数が多いジャンルのリスト
 def genres_with_most_tracks(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT GenreId , COUNT(*) FROM tracks t GROUP BY GenreId ORDER BY COUNT(*) DESC ;"  # ここにSQLクエリを書いてください
     db.execute(query)
     results = db.fetchall()
     return results
 
+artist_count = number_of_artists(db)
+print(artist_count) 
+print(list_of_artists(db))
+print(albums_about_love(db))
+
+duration = 1000000
+track_count = tracks_longer_than(db, duration)
+print(track_count)  # ここで整数をプリント
+
+print(genres_with_most_tracks(db))
 # スクリプトの最後で必ずデータベース接続を閉じる
 conn.close()
+
